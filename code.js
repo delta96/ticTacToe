fdda6b1f1ff9cf1bc9b4cdfade6ceb0e51c5bb84
Ricardo Del Valle <rickydelvalle96@gmail.com>
2023-01-22 18:56:07 -0500

    add logic to reset board after a draw game

--- code.js
+++ code.js
@@ -1,29 +1,41 @@
 const squares = document.querySelectorAll('.square');
 const gameBoard = document.getElementById(board);
-const gameMessage = document.getElementById(message);
+const gameMessage = document.querySelector('#message');
 const showBanner = document.querySelector('.endGame');
+const playButton = document.querySelector('#restartButton');
 let xTurn = true;
 
-squares.forEach(square => {
-    square.addEventListener('click', setMarker, {once: true})
-})
+
+
+function startGame(){
+    
+    squares.forEach(square => {
+        square.classList.remove('x')
+        square.classList.remove('circle')
+        square.innerText = ''
+        square.removeEventListener('click', setMarker)
+        square.addEventListener('click', setMarker, {once: true})
+    })
+    showBanner.classList.remove('banner')
+}
+
+function gameplay(draw) {
+    if (draw) {
+        gameMessage.innerText = 'Draw!'
+    } else {
+        gameMessage.innerText = `${xTurn ? "X's" : "O's"} Wins!`
+    }
+    showBanner.classList.add('banner')
+}
 
 function setMarker(e){
     const square = e.target;
-
-    if (xTurn = !xTurn) {
-        square.innerText = 'O'
-        square.classList.add('circle')
-        xTurn = true
-    } else if (draw()) {
-        gameMessage.innerHTML = 'Draw!'
-        showBanner.classList.add('show')
-        console.log(draw())
-    } else {
-        square.innerText = 'X'
-        square.classList.add('x')
-        xTurn = false
-        console.log(draw())
+    const currentTurn = xTurn ? 'x' : 'circle'
+    placeMarker(square, currentTurn)
+    if (draw()) {
+        gameplay(true)
+    }  else {
+        xTurn = !xTurn
     }
 
 }
@@ -33,3 +45,18 @@ function draw() {
         return square.classList.contains('x') || square.classList.contains('circle')
     })
 }
+
+function placeMarker(square, currentTurn) {
+    if (currentTurn == 'circle'){
+        square.classList.add(currentTurn)
+        square.innerText = 'O'
+    } else {
+        square.classList.add(currentTurn)
+        square.innerText = 'X'
+    }
+}
+
+playButton.addEventListener('click', startGame)
+
+
+startGame()
